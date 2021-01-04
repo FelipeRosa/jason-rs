7104231f6e417278317cdac21bac6977230774ae
Felipe Rosa <felipe.sgrosa@gmail.com>
2021-01-03 22:32:43 -0300

    Add requests examples

---
+++ examples/requests.rs
@@ -0,0 +1,19 @@
+use jason::{https::Client, transport::Transport, ProtocolVersion, RequestId};
+
+#[tokio::main]
+async fn main() {
+    let client = Client::new("https://change/me").expect("failed creating client");
+
+    let res: jason::Response<String> = client
+        .request(jason::Request {
+            jsonrpc: ProtocolVersion::TwoPointO,
+            id: RequestId::Number(1),
+            method: "some_method".to_owned(),
+            params: vec!["param"],
+        })
+        .expect("failed serializing request")
+        .await
+        .expect("request failed");
+
+    println!("{:?}", res);
+}
